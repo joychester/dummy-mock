6bc537be7a901550b9bdb48e8ab91847361c3561
Cheng Chi (@joychester) <joychester@gmail.com>
2020-07-13 16:35:36 -0700

    Update http_node_server.js

--- NodeAgent_benchmark/http_node_server.js
+++ NodeAgent_benchmark/http_node_server.js
@@ -14,7 +14,7 @@ const defaultAgent = new http.Agent({
 
 const optimaldefaultAgent = new http.Agent({
   keepAlive: true, // Keep sockets around in a pool to be used by other requests in the future
-  keepAliveMsecs: 30000, // specifies the initial delay for TCP Keep-Alive packets
+  keepAliveMsecs: 30000, // specifies the initial delay for TCP Keep-Alive packets, really works???
   maxFreeSockets: 10, // Maximum number of sockets to leave open in a free state, which not turn into TIME_WAIT from ESTABLISHED and keep connection reusable, until idle timeout reached. Max Keepalive sockets??
   maxSockets: 100, // Maximum number of sockets to allow per host
   timeout: 60000, // Socket Idle timeout in milliseconds, Client Side TCP Connection will be from ESTABLISHED to TIME_WAIT
@@ -23,7 +23,7 @@ const optimaldefaultAgent = new http.Agent({
 
 const keepaliveAgent = new AgentKA({
   keepAlive: true, // Keep sockets around in a pool to be used by other requests in the future
-  keepAliveMsecs: 30000, // specifies the initial delay for TCP Keep-Alive packets, really works???
+  keepAliveMsecs: 30000, // specifies the initial delay for TCP Keep-Alive packets
   maxSockets: 100, // Maximum number of sockets to allow per host
   maxFreeSockets:10, // Maximum number of sockets (per host) to leave open in a free state
   timeout: 60000, // Sets the working socket to timeout after timeout milliseconds of inactivity on the working socket
