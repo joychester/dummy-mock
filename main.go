217045b65d084b7833fec24b4dc7ff372b91274c
Cheng Chi (@joychester) <joychester@gmail.com>
2021-06-16 10:48:02 -0700

    Update main.go

--- main.go
+++ main.go
@@ -12,9 +12,10 @@ import (
 	"github.com/valyala/fasthttp"
 )
 
-var searchMock []byte
+var mockGet []byte
+var mockPost []byte
 
-func Mock(ctx *fasthttp.RequestCtx) {
+func MockGet(ctx *fasthttp.RequestCtx) {
 	ctx.Response.Header.Set("Content-Type", "application/json")
 	// To-DO: move this as a param
 	var duration int
@@ -27,22 +28,45 @@ func Mock(ctx *fasthttp.RequestCtx) {
 	time.Sleep(time.Duration(duration) * time.Millisecond)
 	ctx.Response.Header.Set("X-Server-Time", strconv.Itoa(duration))
 
-	ctx.Write(searchMock)
+	ctx.Write(mockGet)
+}
+
+func MockPost(ctx *fasthttp.RequestCtx) {
+	ctx.Response.Header.Set("Content-Type", "application/json")
+	// To-DO: move this as a param
+	var duration int
+	if os.Args[1] != "" {
+		duration, _ = strconv.Atoi(os.Args[1])
+	} else {
+		duration = 250
+	}
+
+	time.Sleep(time.Duration(duration) * time.Millisecond)
+	ctx.Response.Header.Set("X-Server-Time", strconv.Itoa(duration))
+
+	ctx.Write(mockPost)
 }
 
 func main() {
 
-	m, e := ioutil.ReadFile("/app/mocks/response.json")
-	if e != nil {
-		fmt.Printf("Error reading mock file: %v\n", e)
+	mGet, e1 := ioutil.ReadFile("/app/mocks/response-GET.json")
+	mPost, e2 := ioutil.ReadFile("/app/mocks/response-POST.json")
+	if e1 != nil {
+		fmt.Printf("Error reading mock file: %v\n", e1)
+		os.Exit(1)
+	} else if e2 != nil {
+		fmt.Printf("Error reading mock file: %v\n", e2)
 		os.Exit(1)
 	}
-	searchMock = m
+
+	mockGet = mGet
+	mockPost = mPost
 
 	fmt.Println("starting...")
 
 	router := fasthttprouter.New()
-	router.GET("/", Mock)
+	router.GET("/", MockGet)
+	router.POST("/", MockPost)
 
 	log.Fatal(fasthttp.ListenAndServe(":80", router.Handler))
 
